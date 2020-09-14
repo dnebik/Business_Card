94b7b82fff7a8a2f745a9fb7dabeabb0064cc1e2
dneb <ya.dneb@yandex.ru>
2020-09-14 23:44:08 +0300

    tamplate

--- views/site/index.php
+++ views/site/index.php
@@ -1,3 +1,10 @@
-<div class="container">
-    <h1>Онлайн конструктор резюме</h1>
+<div class="header tHead">
+    <img class="tHead-img" src="/img/template/example_card.jpeg">
+    <div class="tHead-space"></div>
+    <div class="tHead-triangle">
+        <div class="tHead-text">
+            <b><span>Ваше резюме онлайн</span></b>
+            <button class="btn btn-success tHead-button"><span>Создать резюме</span></button>
+        </div>
+    </div>
 </div>
