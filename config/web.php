5bf375523ea170cc50b4150bc76dcb61fd698cd7
dneb <ya.dneb@yandex.ru>
2020-09-14 23:54:18 +0300

    site index

--- config/web.php
+++ config/web.php
@@ -7,6 +7,7 @@ $config = [
     'id' => 'basic',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
+//    'defaultRoute' => '/profile/index',
     'aliases' => [
         '@bower' => '@vendor/bower-asset',
         '@npm'   => '@vendor/npm-asset',
@@ -15,6 +16,7 @@ $config = [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => '-0f9kN28HDcynULZKpeAoYbTs4_Nw4TP',
+            'baseurl' => '',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
@@ -43,14 +45,13 @@ $config = [
             ],
         ],
         'db' => $db,
-        /*
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
+
             ],
         ],
-        */
     ],
     'params' => $params,
 ];
--- web/js/main.js
+++ web/js/main.js
@@ -1,25 +1,17 @@
 jQuery(document).ready(function($) {
-
-
     /*======= Skillset *=======*/
-    
-    
     $('.level-bar-inner').css('width', '0');
-    
     $(window).on('load', function() {
-
         $('.level-bar-inner').each(function() {
-        
             var itemWidth = $(this).data('level');
-            
             $(this).animate({
                 width: itemWidth
             }, 800);
-            
         });
-
     });
-   
-    
-
 });
+
+$(window).on('resize', function() {
+    $('.tHead-space').width($(window).width() - 950);
+    console.log($(window).width());
+})
